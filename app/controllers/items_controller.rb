aad3e5ff6dfbc7d88b9eb935c32b6e0086eb2a3d
Chirag Shah <chiragshah321@gmail.com>
2017-07-28 19:40:45 +0000

    completed to-do items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,6 +14,16 @@ class ItemsController < ApplicationController
     end
   end
   
+    def destroy
+        @item = Item.find(params[:id])
+            
+        @item.destroy
+
+        respond_to do |format|
+            format.html 
+            format.js 
+        end
+    end
   
   private 
   
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
    end
    
   get 'items/create'  
+  
+  get 'items/delete'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
